e74ae366aa8f3d9ae9e9be2830e981b073e48850
Eduardo Lucas de Medeiros Silva <20221101110056@ifrn.local>
2024-01-08 14:55:25 -0300

    Ultimo commit

--- p_projeto/classe_cinema.py
+++ p_projeto/classe_cinema.py
@@ -1,6 +1,5 @@
-from tkinter import *
 import sqlite3
-
+import messagebox
 class Cinema:
     def __init__(self):
         self.lista_ttl = ['JOGOS VORAZES: A CANTIGA DOS PÁSSAROS E DAS SERPENTES','LEO','MUSSUM:O FILMIS','PATRULHA CANINA:UM FILME SUPERPODEROSO']
@@ -9,28 +8,28 @@ class Cinema:
         self.lista_data = ['15 de novembro de 2023','22 de novembro de 2023','2 de novembro de 2023 ','5 de outubro de 2023']
         self.lista_drt = ['Francis Lawrence','Robert Smigel','Silvio Guindane','Cal Brunker']
 
-
-  
-
-  
     def Consultar_filme(self):
-        self.conexao = sqlite3.connect('tabela_filmes.db')
-        self.sql = self.conexao.cursor()
-        self.sql.execute('SELECT * FROM filmes')
-        self.pcrfilme = self.sql.fetchall()
-        self.dicionario_filmes = {}
-        for i in self.pcrfilme:
-            self.dicionario_filmes[i[0]] = [i[1],i[2],i[3],i[4]]
-        self.conexao.commit()
-        self.conexao.close()
-        return self.dicionario_filmes
-        
+        try:
+            self.conexao = sqlite3.connect('tabela_filmes.db')
+            self.sql = self.conexao.cursor()
+            self.sql.execute('SELECT * FROM filmes')
+            self.pcrfilme = self.sql.fetchall()
+            self.dicionario_filmes = {}
+            for i in self.pcrfilme:
+                self.dicionario_filmes[i[0]] = [i[1],i[2],i[3],i[4]]
+            self.conexao.commit()
+            self.conexao.close()
+            return self.dicionario_filmes
+        except:
+            messagebox.showwarning('ERRO')
     def Adicionar_filmes(self):
-        self.conexao = sqlite3.connect('tabela_filmes.db')
-        self.sql = self.conexao.cursor()
-        for i in range(len(self.lista_ttl)):
-            self.sql.execute("INSERT INTO filmes (titulo,categoria,duracao,data,diretor) VALUES (?,?,?,?,?)",(self.lista_ttl[i],self.lista_ctg[i],self.lista_drc[i],self.lista_data[i],self.lista_drt[i],))
-        self.conexao.commit()
-        self.conexao.close()
+        try:          
+            self.conexao = sqlite3.connect('tabela_filmes.db')
+            self.sql = self.conexao.cursor()
+            for i in range(len(self.lista_ttl)):
+                self.sql.execute("INSERT INTO filmes (titulo,categoria,duracao,data,diretor) VALUES (?,?,?,?,?)",(self.lista_ttl[i],self.lista_ctg[i],self.lista_drc[i],self.lista_data[i],self.lista_drt[i],))
+            self.conexao.commit()
+            self.conexao.close()
+        except:
+             messagebox.showwarning('ERRO')
 add = Cinema()
-add.Adicionar_filmes()
